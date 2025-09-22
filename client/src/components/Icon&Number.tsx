2f2b7a0d44765116934b064ae9ffe4a8f5945bfa
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-22 16:22:02 -0300

    component and test for test CI

---
+++ client/src/components/Icon&Number.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+interface Props {
+    src: string;
+    number: string | number;
+}
+
+export default function IconAndNumber ({src, number}: Props) {
+    return (
+        <div className="flex flex-row gap-1 scale-75 sm:scale-100">
+            <Image
+                src={src}
+                alt="icono"
+                width={20}
+                height={20}
+            />
+            <p className="text-gray-500 text-sm">{number}</p>
+        </div>
+    )
+}
